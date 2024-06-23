649d0e0cc6d5d098383b0686e1999cbfd772126e
unwireddd <unwired@tutanota.de>
2024-06-23 23:51:06 +0000

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,28 @@
+module mymod
+
+go 1.22.3
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/jaypipes/ghw v0.12.0 // indirect
+	github.com/jaypipes/pcidb v1.0.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/shirou/gopsutil/v4 v4.24.5 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	howett.net/plist v1.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/jaypipes/ghw"
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/host"
+	"github.com/shirou/gopsutil/v4/mem"
+)
+
+// Converting bytes to megabytes
+func conv(unit uint64) float64 {
+	result := unit / 1000000
+	return float64(result)
+}
+
+func main() {
+
+	bgDark := color.New(color.BgHiBlack)
+	bgRed := color.New(color.BgHiRed)
+	bgGreen := color.New(color.BgHiGreen)
+	bgYellow := color.New(color.BgHiYellow)
+	bgBlue := color.New(color.BgBlue)
+	bgPurple := color.New(color.BgHiMagenta)
+	bgAqua := color.New(color.BgHiBlue)
+	bgWhite := color.New(color.BgHiWhite)
+
+	// Obtaining the CPU info
+	cpu, _ := cpu.Info()
+	cpuModel := cpu[0].ModelName
+	//fmt.Println(cpuModel)
+
+	// Obtaining the hostname info
+	host, _ := host.Info()
+	hostname := host.Hostname
+	uptime := host.Uptime
+	uptime = uptime / 3600
+
+	// Obtaining the OS and kernel version info
+	osversion := host.Platform
+	operating := host.OS
+	kernel := host.KernelVersion
+	kernelArch := host.KernelArch
+
+	// Obtaining the GPU info
+	graphs, _ := ghw.GPU(ghw.WithDisableWarnings())
+	graphInfo := graphs.GraphicsCards
+	gpuout := graphInfo[1]
+	graphFinal := gpuout.DeviceInfo.Product.Name
+
+	// Obtaining the BIOS info
+	bios, _ := ghw.BIOS()
+	biosVen := bios.Vendor
+
+	// Obtaining the hard drive info
+	drive, _ := ghw.Block()
+	test2 := drive.Disks[0]
+	driveFinal := test2.Model
+
+	// Obtaining the timezone info
+	timezone, _ := time.Now().Local().Zone()
+
+	// Obtaining the memory info
+	virmem, _ := mem.VirtualMemory()
+	swap, _ := mem.SwapMemory()
+	totalmem := virmem.Total
+	usedmem := virmem.Used
+	percentused := virmem.UsedPercent
+	totalswap := swap.Total
+
+	// Printing out the basic info
+	var distro string
+	kolor := color.New(color.BgBlack)
+	switch {
+	case osversion == "arch":
+		distro = "Ascii/ascii"
+		blue := color.New(color.FgHiBlue)
+		kolor = blue.Add(color.Bold)
+	case osversion == "artix":
+		distro = "Ascii/artix"
+		blue := color.New(color.FgHiBlue)
+		kolor = blue.Add(color.Bold)
+	case osversion == "debian":
+		distro = "Ascii/debian"
+		red := color.New(color.FgHiRed)
+		kolor = red.Add(color.Bold)
+	case osversion == "fedora":
+		distro = "Ascii/fedora"
+		blue := color.New(color.FgHiBlue)
+		kolor = blue.Add(color.Bold)
+	case osversion == "gentoo":
+		distro = "Ascii/gentoo"
+		purple := color.New(color.FgHiMagenta)
+		kolor = purple.Add(color.Bold)
+	case osversion == "popos":
+		distro = "Ascii/popos"
+		grey := color.New(color.FgHiWhite)
+		kolor = grey.Add(color.Bold)
+	case osversion == "slackware":
+		distro = "Ascii/slackware"
+		blue := color.New(color.FgHiBlue)
+		kolor = blue.Add(color.Bold)
+	case osversion == "ubuntu":
+		distro = "Ascii/ubuntu"
+		yellow := color.New(color.FgHiYellow)
+		kolor = yellow.Add(color.Bold)
+	case osversion == "void":
+		distro = "Ascii/void"
+		green := color.New(color.FgHiGreen)
+		kolor = green.Add(color.Bold)
+	default:
+		distro = "Ascii/linux"
+		grey := color.New(color.FgHiWhite)
+		kolor = grey.Add(color.Bold)
+
+	}
+	fmt.Println("")
+	ascii, _ := os.Open(distro)
+	scanner := bufio.NewScanner(ascii)
+	for scanner.Scan() {
+		kolor.Println(scanner.Text())
+	}
+
+	kolor.Println("")
+	kolor.Print("Hostname: ")
+	fmt.Println(hostname)
+
+	kolor.Print("Operating System: ")
+	fmt.Println(osversion, operating)
+
+	kolor.Print("Kernel: ")
+	fmt.Println(kernel, kernelArch)
+
+	kolor.Print("RAM: ")
+	fmt.Print(conv(usedmem), "M", " / ", conv(totalmem), "M", " (", int(percentused), "%", ")")
+	fmt.Println()
+
+	kolor.Print("Swap: ")
+	fmt.Print(conv(totalswap), "M")
+	fmt.Println()
+
+	kolor.Print("Processor: ")
+	fmt.Println(cpuModel)
+
+	kolor.Print("Graphics: ")
+	fmt.Println(graphFinal)
+
+	kolor.Print("Hard Drive: ")
+	fmt.Println(driveFinal)
+
+	kolor.Print("BIOS: ")
+	fmt.Println(biosVen)
+
+	kolor.Print("Uptime: ")
+	fmt.Println(uptime, "hours")
+	kolor.Print("Timezone: ")
+	fmt.Println(timezone)
+	bgDark.Print("  ")
+	bgRed.Print("  ")
+	bgGreen.Print("  ")
+	bgYellow.Print("  ")
+	bgBlue.Print("  ")
+	bgPurple.Print("  ")
+	bgAqua.Print("  ")
+	bgWhite.Print("  ")
+	fmt.Println("")
+	kolor.Println("")
+
+}
